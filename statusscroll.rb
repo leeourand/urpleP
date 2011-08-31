750f25b5b378d0e6a36a0b245803bda589694b9b
Lee <lee@gastrostation2.(none)>
2011-08-31 11:14:00 -0400

    How beautifully annoying

---
+++ statusscroll.rb
@@ -0,0 +1,28 @@
+class Marquee
+  
+  attr_accessor :status, :buffer_size
+
+  def initialize(status = "test", buffer_size = 20)
+    @buffer_size = buffer_size 
+    @status = status.split("")
+  end
+
+  def print(str)
+    `purple-remote "setstatus?message=#{str}"`
+  end
+
+  def scroll
+    loop do
+      marquee = status[0, buffer_size]
+      print(marquee.to_s)
+      
+      carry = status.shift
+      status << carry
+      sleep 0.1
+    end
+  end
+
+end
+
+m = Marquee.new("onetwothreefourfivesizeseveneightnineten")
+m.scroll
